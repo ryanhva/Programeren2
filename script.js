465f010e68e2951976cb3c0f411da4d08b47fead
ryanhva <ryan.yeung@hva.nl>
2024-03-24 22:57:53 +0100

    24/3

--- script.js
+++ script.js
@@ -1,18 +1,59 @@
-console.log();
-let getal = 0;
+const playerText = document.querySelector("#playerText")
+const computerText = document.querySelector("#computerText")
+const resultText = document.querySelector("#resultText")
+const choiceButtons = document.querySelectorAll(".choiceButton")
 
-const plusKnop = document.querySelector("#plus");
-const minKnop = document.querySelector("#min");
-let getalVal = document.querySelector("h1")
+let player;
+let computer;
+let result;
 
-function verhoogGetal(){
-    getal = getal + 1;
-    console.log("getal: " + getal);
-    getalVal.textContent = getal;
-}
+choiceButtons.forEach(button => button.addEventListener("click",() => {
+   
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}` ;
+    computerText.textContent = `Computer: ${computer}` ;
+    resultText.textContent = checkWinner();
 
-function verlaagGetal(){
-    getal = getal - 1;
-    console.log("getal: " + getal);
-    getalVal.textContent = getal;
+}));
+
+function computerTurn(){
+
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randNum){
+        case 1:
+            computer = "ROCK";
+            break;
+        case 2:
+            computer = "PAPER";
+            break;
+        case 3:
+            computer = "SCISSORS";
+            break;
+    }
+} 
+
+function checkWinner(){
+    if(player == computer){
+        return "Draw"
+    }
+    else if(computer == "ROCK"){
+        return (player == "PAPER") ? "You win!" : "You Lose!"
+    }
+    else if(computer == "PAPER"){
+        return (player == "SCISSORS") ? "You win!" : "You Lose!"
+    }
+    else if(computer == "SCISSORS"){
+        return (player == "ROCK") ? "You win!" : "You Lose!"
+    }
+
+    switch(result){
+        case "You win!":
+            resultDisplay.classList.add("greenText");
+            break;
+        case "You lose!":
+            resultDisplay.classList.add("redText");
+            break;
+    }
 }

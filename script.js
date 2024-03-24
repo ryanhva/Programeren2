4c6fc7c9da14354a4e6c8b4b2c1b57b40288ac65
ryanhva <ryan.yeung@hva.nl>
2024-03-24 23:40:43 +0100

    final update

--- script.js
+++ script.js
@@ -2,6 +2,79 @@ const playerText = document.querySelector("#playerText")
 const computerText = document.querySelector("#computerText")
 const resultText = document.querySelector("#resultText")
 const choiceButtons = document.querySelectorAll(".choiceButton")
+const playerScoreDisplay = document.querySelector("#playerScoreDisplay")
+const computerScoreDisplay = document.querySelector("#computerScoreDisplay")
+
+let playerScore = 0;
+let computerScore = 0;
+
+
+
+choiceButtons.forEach(button => button.addEventListener("click",() => {
+   
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}` ;
+    computerText.textContent = `Computer: ${computer}` ;
+    resultText.textContent = checkWinner();
+
+}));
+
+function computerTurn(){
+
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randNum){
+        case 1:
+            computer = "ROCK";
+            break;
+        case 2:
+            computer = "PAPER";
+            break;
+        case 3:
+            computer = "SCISSORS";
+            break;
+    }
+} 
+
+function checkWinner(){
+    if(player == computer){
+        result = "Draw";
+    }
+    else if(computer == "ROCK"){
+        result = (player == "PAPER") ? "You win!" : "You Lose!";
+    }
+    else if(computer == "PAPER"){
+        result = (player == "SCISSORS") ? "You win!" : "You Lose!";
+    }
+    else if(computer == "SCISSORS"){
+        result = (player == "ROCK") ? "You win!" : "You Lose!";
+    }
+
+    switch(result){
+        case "You win!":
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore;
+        break
+
+        case "You Lose!":
+            computerScore++;
+            computerScoreDisplay.textContent = computerScore;
+            break
+    }
+    return result;
+}
+
+
+/*const playerText = document.querySelector("#playerText")
+const computerText = document.querySelector("#computerText")
+const resultText = document.querySelector("#resultText")
+const choiceButtons = document.querySelectorAll(".choiceButton")
+const playerScoreDisplay = document.querySelector("#playerScoreDisplay")
+const computerScoreDisplay = document.querySelector("#computerScoreDisplay")
+
+let playerScore = 0;
+let computerScore = 0;
 
 let player;
 let computer;
@@ -50,10 +123,13 @@ function checkWinner(){
 
     switch(result){
         case "You win!":
-            resultDisplay.classList.add("greenText");
-            break;
-        case "You lose!":
-            resultDisplay.classList.add("redText");
-            break;
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore;
+        break
+
+        case "You Lose!":
+            computerScore++;
+            computerScoreDisplay.textContent = computerScore;
+            break
     }
-}
+}*/

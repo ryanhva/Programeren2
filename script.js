89e916866bd7401d7fcb3225b8d5fb0498775cee
ryanhva <ryan.yeung@hva.nl>
2024-03-25 00:11:36 +0100

    final update

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+// met de hulp van youtube https://youtu.be/3uKdQx-SZ5A?si=meXiY0MH4lRbx2LS
 const playerText = document.querySelector("#playerText")
 const computerText = document.querySelector("#computerText")
 const resultText = document.querySelector("#resultText")
 const choiceButtons = document.querySelectorAll(".choiceButton")
 const playerScoreDisplay = document.querySelector("#playerScoreDisplay")
 const computerScoreDisplay = document.querySelector("#computerScoreDisplay")
+const drawScoreDisplay = document.querySelector("#drawScoreDisplay")
 
 let playerScore = 0;
 let computerScore = 0;
-
-
+let drawScore = 0;
+let player;
+let computer;
+let result;
 
 choiceButtons.forEach(button => button.addEventListener("click",() => {
    
@@ -61,75 +65,13 @@ function checkWinner(){
             computerScore++;
             computerScoreDisplay.textContent = computerScore;
             break
+
+        case "Draw":
+            drawScore++;
+            drawScoreDisplay.textContent = drawScore;
+            break
     }
     return result;
 }
 
 
-/*const playerText = document.querySelector("#playerText")
-const computerText = document.querySelector("#computerText")
-const resultText = document.querySelector("#resultText")
-const choiceButtons = document.querySelectorAll(".choiceButton")
-const playerScoreDisplay = document.querySelector("#playerScoreDisplay")
-const computerScoreDisplay = document.querySelector("#computerScoreDisplay")
-
-let playerScore = 0;
-let computerScore = 0;
-
-let player;
-let computer;
-let result;
-
-choiceButtons.forEach(button => button.addEventListener("click",() => {
-   
-    player = button.textContent;
-    computerTurn();
-    playerText.textContent = `Player: ${player}` ;
-    computerText.textContent = `Computer: ${computer}` ;
-    resultText.textContent = checkWinner();
-
-}));
-
-function computerTurn(){
-
-    const randNum = Math.floor(Math.random() * 3) + 1;
-
-    switch(randNum){
-        case 1:
-            computer = "ROCK";
-            break;
-        case 2:
-            computer = "PAPER";
-            break;
-        case 3:
-            computer = "SCISSORS";
-            break;
-    }
-} 
-
-function checkWinner(){
-    if(player == computer){
-        return "Draw"
-    }
-    else if(computer == "ROCK"){
-        return (player == "PAPER") ? "You win!" : "You Lose!"
-    }
-    else if(computer == "PAPER"){
-        return (player == "SCISSORS") ? "You win!" : "You Lose!"
-    }
-    else if(computer == "SCISSORS"){
-        return (player == "ROCK") ? "You win!" : "You Lose!"
-    }
-
-    switch(result){
-        case "You win!":
-        playerScore++;
-        playerScoreDisplay.textContent = playerScore;
-        break
-
-        case "You Lose!":
-            computerScore++;
-            computerScoreDisplay.textContent = computerScore;
-            break
-    }
-}*/
